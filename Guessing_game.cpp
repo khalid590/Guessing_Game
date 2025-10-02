72eef80383d284b1ac59f6df26246730bed76318
khalid590 <khalidwasif590@gmail.com>
2025-10-02 13:31:36 +0600

    Game Uploaded

---
+++ Guessing_game.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+   while(1){
+
+     int randNumber, guessNumber;
+    cout << "Enter your Guessing Number from 1 to 5: " <<endl;
+    cin >> guessNumber;
+
+    randNumber = rand() % 5 + 1;
+
+    if(guessNumber == randNumber){
+        cout << "Woooo! You Won." <<endl;
+    }
+
+    else{
+        cout << "You loose! Try again." <<endl;
+        cout << "Random Number was: " <<randNumber << endl <<endl;
+    }
+   }
+
+
+}
